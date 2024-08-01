79875821deda94b2b773a4aeaaf3cc3731607fd8
Lee ChanMi <anytime0224@gmail.com>
2024-08-02 02:33:11 +0900

    :white_check_mark: [TEST] 채용공고 수정 단위 테스트 작성

--- src/test/java/com/wanted/server/domain/recruit/RecruitTest.java
+++ src/test/java/com/wanted/server/domain/recruit/RecruitTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import com.wanted.server.application.service.command.RecruitCreateCommand;
+import com.wanted.server.application.service.command.RecruitUpdateCommand;
 import com.wanted.server.common.exception.model.ValidationException;
 import com.wanted.server.common.response.StatusCode;
 import com.wanted.server.stub.RecruitStub;
@@ -50,6 +51,45 @@ class RecruitTest extends UnitTest {
                             command.content(),
                             -1,
                             command.companyId())
+            ).isInstanceOf(ValidationException.class).hasMessage(StatusCode.INVALID_COMPENSATION_ERROR.getMessage());
+        }
+    }
+
+    @DisplayName("채용공고 수정 테스트")
+    @Nested
+    class Update {
+
+        @DisplayName("[성공] 채용공고 수정에 성공한다.")
+        @Test
+        void success() {
+            RecruitUpdateCommand command = RecruitStub.getRecruitUpdateCommand();
+
+            Recruit updated = RecruitStub.getRecruit().update(
+                    command.position(),
+                    command.stack(),
+                    command.content(),
+                    command.compensation()
+            );
+
+            assertThat(updated).isNotNull();
+            assertThat(updated.getPosition()).isEqualTo(command.position());
+            assertThat(updated.getStack()).isEqualTo(command.stack());
+            assertThat(updated.getContent()).isEqualTo(command.content());
+            assertThat(updated.getCompensation()).isEqualTo(command.compensation());
+        }
+
+        @DisplayName("[실패] 보상금 금액이 0보다 작은 경우 채용공고 수정에 실패한다.")
+        @Test
+        void fail_update_when_compensation_lesser_than_zero() {
+            RecruitUpdateCommand command = RecruitStub.getRecruitUpdateCommand();
+            Recruit recruit = RecruitStub.getRecruit();
+
+            assertThatThrownBy(() ->
+                    recruit.update(
+                            command.position(),
+                            command.stack(),
+                            command.content(),
+                            -1)
             ).isInstanceOf(ValidationException.class).hasMessage(StatusCode.INVALID_COMPENSATION_ERROR.getMessage());
         }
     }
--- src/test/java/com/wanted/server/stub/RecruitStub.java
+++ src/test/java/com/wanted/server/stub/RecruitStub.java
@@ -1,8 +1,22 @@
 package com.wanted.server.stub;
 
 import com.wanted.server.application.service.command.RecruitCreateCommand;
+import com.wanted.server.application.service.command.RecruitUpdateCommand;
+import com.wanted.server.domain.recruit.Recruit;
 
 public class RecruitStub {
+
+    public static Recruit getRecruit() {
+        RecruitCreateCommand command = getRecruitCreateCommand();
+
+        return Recruit.create(
+                command.position(),
+                command.stack(),
+                command.content(),
+                command.compensation(),
+                command.companyId()
+        );
+    }
 
     public static RecruitCreateCommand getRecruitCreateCommand() {
         return RecruitCreateCommand.builder()
@@ -11,6 +25,16 @@ public class RecruitStub {
                 .content("백엔드 개발자를 모집합니다. 팀에 합류하여 함께 성장할 인재를 찾고 있습니다.")
                 .compensation(500000)
                 .companyId(1L)
+                .build();
+    }
+
+    public static RecruitUpdateCommand getRecruitUpdateCommand() {
+        return RecruitUpdateCommand.builder()
+                .id(1L)
+                .position("디자이너")
+                .stack("figma")
+                .content("디자이너를 모집합니다. 팀에 합류하여 함께 성장할 인재를 찾고 있습니다.")
+                .compensation(1000000)
                 .build();
     }
 }
