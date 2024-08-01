083e11d368a85e6c6088c092cc3142c12467f623
05AM <anytime0224@gmail.com>
2024-08-02 00:52:25 +0900

    :white_check_mark: [FEATURE] 단위 테스트 템플릿 정의

---
+++ src/test/java/com/wanted/server/template/UnitTest.java
@@ -0,0 +1,8 @@
+package com.wanted.server.template;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class UnitTest {
+}
