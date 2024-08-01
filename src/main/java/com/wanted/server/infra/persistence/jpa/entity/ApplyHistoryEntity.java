35b4ecce43af15dd4e6cda12ce62fd44df4d59d8
Lee ChanMi <anytime0224@gmail.com>
2024-08-02 04:53:25 +0900

    :sparkles: [FEATURE] 채용공고 엔티티 정의

---
+++ src/main/java/com/wanted/server/infra/persistence/jpa/entity/ApplyHistoryEntity.java
@@ -0,0 +1,37 @@
+package com.wanted.server.infra.persistence.jpa.entity;
+
+import com.wanted.server.common.model.BaseTimeEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "apply_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class ApplyHistoryEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "recruit_id")
+    private RecruitEntity recruit;
+}
